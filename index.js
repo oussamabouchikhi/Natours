78db591786b2d04b2f8ae26bf7c2a0926c8e089a
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-20 16:18:22 +0100

    send tours-simple json data

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+const fs = require("fs");
 const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.status(200).json({message: "Hello from the server side!", app: "Natours"});
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get("/api/v1/tours", (req, res)=>{
+    res
+        .status(200)
+        .json(tours);
     
 });
 
-app.get("/", (req, res)=>{
-    console.log("You can post to this endpoint");
-    
-});
-
-app.listen(3000, (req, res)=> console.log("App running at: localhost:3000"));
+const PORT = 3000;
+app.listen(PORT, ()=> console.log(`App running at: localhost:${PORT}`));
