bdf97ec82c3db8e94412e630c5038712022de361
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-05 14:13:24 +0100

    Handle POST Requests

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 const express = require('express');
+const { send } = require("process");
 
 const app = express();
+
+// Middleware to get data from request
+app.use(express.json());
 
 // Read tours-simple.json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
@@ -17,6 +21,26 @@ app.get("/api/v1/tours", (req, res)=>{
             }
         });
     
+});
+
+app.post("/api/v1/tours", (req, res)=>{
+    // Generate new tour id from last tour id + 1 
+    const newId = tours[tours.length -1].id + 1;
+    // Merge id with data from req in 1 object
+    const newTour = Object.assign({id: newId}, req.body);
+
+    // ADD NEW item to tours array then convert it to json then add it to tours file
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, err => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        });
+    
+    });
+    res.send("Tour created successfully");
 });
 
 const PORT = 3000;
