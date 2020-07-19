9b757f8dd6f09595bb8c1dc21c1972b869900b1d
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-19 16:35:25 +0100

    Setup environment variables, eslint & prettier

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
 const app = require("./index");
 
+// console.log(process.env);
+
 /** Start server **/
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log(`App running at: localhost:${PORT}`));
