c6181f0c4a50e0fc9c060fa3ed574029794f2008
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-20 12:38:26 +0100

    Connect to db

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
-
 const app = require("./index");
 
-// console.log(process.env);
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected successfully to DB'));
 
 /** Start server **/
 const PORT = process.env.PORT || 3000;
