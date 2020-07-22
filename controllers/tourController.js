38c28086e33fbb0eb8f054510cecbfbcc2673fc5
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-22 15:00:45 +0100

    Implement Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,20 +4,32 @@ exports.getAllTours = async (req, res) => {
 
   try {
     // BUILD QUERY
+    // 1)- Filtering
     const queryObj = {...req.query};
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(field => delete queryObj[field]);
     console.log(req.query, queryObj);
 
-    const query = Tour.find(queryObj);
+    // 2)- Advanced Filtering
+    /*
+    * As we filter difficulty[gte]=5 we will get an object {difficulty: {gte: 5}}
+    * so we want to replace these filters with MongoDB functions i.e: $gte
+    * \b\b : to match the exact word, g: replace all filters if we have multiple
+    */
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
+    console.log(JSON.parse(queryStr));
+    // EXECUTE QUERY
+    const tours = await query;
+
+
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // EXECUTE QUERY
-    const tours = await query;
 
     // SEND RESPONSE
     res.status(200).json({
