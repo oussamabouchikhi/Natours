81288aeb46db0e02050e66f77203654ef7efbd31
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-08-24 18:24:41 +0100

    Disactivate user account

--- models/userModel.js
+++ models/userModel.js
@@ -40,7 +40,12 @@ const userSchema = new mongoose.Schema({
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
-  passwordResetExpires: Date
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', async function(next) {
@@ -61,6 +66,12 @@ userSchema.pre('save', function() {
 
   // Because saving password might sometimes take much time(after JWT token) we subtract 1sec as a trick
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function(next) {
+  // this points to the current query
+  this.find({ active: { $ne: false } });
   next();
 });
 
