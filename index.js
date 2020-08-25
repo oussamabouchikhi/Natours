9113225425f09f06c76b299bd3772a05c82a0fc6
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-08-25 14:51:38 +0100

    Limit requests rate per hour

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -10,6 +11,15 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit http requests to 100req/hour
+const limiter = rateLimit({
+  max: 100,
+  windowsMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour'
+});
+// Apply this middleware for routes that starts with api
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
