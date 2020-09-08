524b368857ede3d7aeb38b25f83d349f6e802005
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-09-08 12:04:27 +0100

    Build tour details page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,8 +4,7 @@ const catchAsync = require('./../utils/catchAsync');
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1)- Get tour data from collection
   const tours = await Tour.find();
-  // 2)- Build template
-
+  // 2)- Build template (views/overview.pug)
   // 3)- Render tha template usnig tour date from 1)
   res.status(200).render('overview', {
     title: 'All tours',
@@ -13,8 +12,16 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker'
+exports.getTour = catchAsync(async (req, res) => {
+  // 1)- Get tour data from the requested tour (including reviews & guides)
+  const tour = await Tour.findOne({slug: req.params.slug}).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-}
+  // 2)- Build template (views/tour.pug)
+  // 3)- Render tha template usnig tour date from 1)
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+    tour
+  });
+});
