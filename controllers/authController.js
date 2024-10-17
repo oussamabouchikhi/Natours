fbfd9dbabd0a260678c27e38d8e009b1986e89e8
Hadj H. <36198012+elhadjx@users.noreply.github.com>
2024-10-17 16:17:56 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -65,7 +65,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.logout = (req, res) => {
   res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.npw()),
+    expires: new Date(Date.now()),
     httpOnly: true
   });
   res.status(200).json({ status: 'success' });
